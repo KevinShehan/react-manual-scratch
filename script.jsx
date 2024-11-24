314b30aa203dae33ab8b7d6c2c2a3f55768015fc
Kevin <kevin.shehan30@gmail.com>
2024-11-24 18:22:46 +0530

    jsx added

---
+++ script.jsx
@@ -0,0 +1,10 @@
+import {createRoot} from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    React.createElement('div', null, 
+        React.createElement('h1', null, "Hello, World!"),
+        React.createElement('p', null, "This is a simple React app.")
+    )
+);
