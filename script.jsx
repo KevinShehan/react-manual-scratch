24bef213e28ff7aa7e5eb003fe914cd14fbf577d
Kevin <kevin.shehan30@gmail.com>
2024-11-27 23:40:56 +0530

    react nested element

--- script.jsx
+++ script.jsx
@@ -6,6 +6,10 @@ root.render(
     React.createElement('div', null, 
         React.createElement('h1', null, "Hello, World!"),
         React.createElement('p', null, "This is a simple React app.")
+    ),
+      React.createElement('div', null, 
+        React.createElement('h1', null, "Hello, World!"),
+        React.createElement('p', null, "This is a simple React app.")
     )
 );
 
