54e2923256ffb0170e61438e80f0086eea7bec6f
Kevin <kevin.shehan30@gmail.com>
2024-11-28 23:50:06 +0530

    nested 3

--- script.jsx
+++ script.jsx
@@ -10,6 +10,11 @@ root.render(
       React.createElement('div', null, 
         React.createElement('h1', null, "Hello, World!"),
         React.createElement('p', null, "This is a simple React app.")
+    ),
+    ,
+      React.createElement('div', null, 
+        React.createElement('h1', null, "Hello, World!"),
+        React.createElement('p', null, "This is a simple React app.")
     )
 );
 
